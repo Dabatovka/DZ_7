e25553800d07f056b131e4e955768345e6f9a61c
Elena Dabatovka <dabatovka@gmail.com>
2023-03-08 16:10:45 +0300

    1

---
+++ task_1/Program.cs
@@ -0,0 +1,36 @@
+﻿//47. Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+            
+
+Console.Write("Введите количество строк массива: ");
+int rows=Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns=int.Parse(Console.ReadLine());
+
+double[,] array = new double [rows, columns];
+ArrayRandomNumbers(array);
+PrintArray(array);
+
+void ArrayRandomNumbers(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
